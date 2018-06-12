775e48942ec7e13ff7ae95ac7267853638101ed4
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-12 18:06:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -62,12 +62,12 @@ class Song
   
   def self.create_from_filename(file)
     f = file.split(".").first
-    artist = f.split(" - ").first.gsub()
-    song_name = f.split(" - ").last.lstrip
+    artist = f.split(" - ").first.strip!
+    song_name = f.split(" - ").last.strip!
     song = self.new(song_name, artist)
-    song.name = song_name.strip
+    song.name = song_name.strip!
     binding.pry
-    song.artist_name = artist.strip
+    song.artist_name = artist.strip!
     binding.pry
     song.save
     song
