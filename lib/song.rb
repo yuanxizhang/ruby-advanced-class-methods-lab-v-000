2c8c756074b2493a9d0de9398553ec14c97555f3
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-12 15:20:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,13 @@ class Song
     @@all
   end
   
-  def self,create(name, artist_name)
+  def self.create(name, artist_name) 
+    song = self.class.new(name, artist_name)
+    @name = name
+    @artist_name = artist_name
+    self.save
+    song
+  end
 
   def save
     self.class.all << self
