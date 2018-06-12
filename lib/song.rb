f260d86c5ecdc9ecc1250260fa2bf52332803a1b
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-12 15:25:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,10 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
   
-  def initialize
-    
+  def initialize(name = nil, artist_name = nil)
+    @name = name 
+    @artist_name = artist_name
+  end  
 
   def self.all
     @@all
