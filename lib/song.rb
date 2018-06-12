19fd085495d1d36074fce455e9d8198e29892750
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-12 15:49:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,6 +28,12 @@ class Song
   def self.find_by_name(name) 
     @@all.find {|song| song.name == name }
   end
+  
+  def self.find_or_create_by_name(name) 
+    song = self.class.new(name)
+    song.class.all << song
+    song
+  end
 
   def save
     self.class.all << self
