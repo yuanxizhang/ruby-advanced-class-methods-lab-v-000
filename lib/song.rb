7a5997c86c58487ae5398446100ce5395b66bc1f
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-12 16:47:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -62,7 +62,7 @@ class Song
   def self.create_from_filename(file)
     f = file.split(".").first
     artist = f.split("-").first 
-    song_name = f.split("-").last
+    song_name = f.split("-").strip().last
     song = self.create_by_name(song_name)
     song.name = song_name
     song.artist_name = artist 
