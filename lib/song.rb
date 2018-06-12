51799689a3b55761e4a1fe4ab764521737166f8d
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-12 15:24:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,22 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+  def initialize
+    
 
   def self.all
     @@all
   end
   
   def self.create 
-    self.class.new 
+    song = self.class.new 
+    song.save
+    song
   end
   
-  def self.create(name) 
-    song = self.class.new(name, artist_name)
+  def self.new_by_name(name) 
+    song = self.class.new(name)
     @name = name
     @artist_name = artist_name
     self.save
